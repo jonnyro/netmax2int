45343334307fe3aa834e4575ea12b783061e20fc
Jonathan S. Romero <jonnyro@jonnyro.com>
2011-04-05 17:59:19 -0400

    Add top level module for constants that can be shared between client and server.
    More work on layout of server process.

--- server/server.py
+++ server/server.py
@@ -8,26 +8,27 @@ while running
   Update job status for results. Removing from queue
 """
 import xmlrpclib
+sys.path.append("../common")
+from common import JOB_PENDING_START, JOB_IN_PROGRESS, JOB_FAILED, JOB_COMPLETE, JOB_NOT_FOUND
+import uuid
+
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 
-def is_even(n):
-    return n%2 == 0
-
-server = SimpleXMLRPCServer(("localhost", 8000))
-print "Listening on port 8000..."
-server.register_function(is_even, "is_even")
-server.serve_forever()
 """
 1. get_job_output_dir
 2. get_job_submission_dir
 3. setup_job(input_format_name,output_format_name) returns (jobid)
 4. start_job(job_id)
-5. query_job_status(job_id) returns (JOB_PENDING_START,JOB_IN_PROGRESS,JOB_FAILED,JOB_ COMPLETE)
+5. query_job_status(job_id) returns (JOB_PENDING_START,JOB_IN_PROGRESS,JOB_FAILED,JOB_COMPLETE,JOB_NOT_FOUND)
 """
 
 
 class ExportJob:
-	def __init__:
+	def __init__(self,in_format,out_format):
+		self.job_id = str(uuid.uuid4())
+		self.in_format = in_format
+		self.out_format = out_format
+		self.job_status = JOB_PENDING_START
 	
 
 jobs = []
@@ -39,13 +40,28 @@ def get_job_submission_dir():
 	pass
 	
 def setup_job(input_format_name,output_format_name):
-	pass
+	global jobs
+	job = ExportJob(input_format_name,output_format_name)
+	jobs.append(job)
+	return job.job_id()
 	
 def start_job(job_id):
 	pass
 
 def query_job_status(job_id):
-	pass
+	global jobs
+	
+	for job in jobs:
+		if (job.job_id == job_id):
+			return job.job_status
+	
+	return JOB_NOT_FOUND
+		
 
 
-
+if __name__ == "__main__":
+	
+	server = SimpleXMLRPCServer(("localhost", 8000))
+	print "Listening on port 8000..."
+	#server.register_function(is_even, "is_even")
+	server.serve_forever()
