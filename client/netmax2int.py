3d52bd31a2980231b4fee2c031b07bcd4d99918a
Jonathan S. Romero <jonnyro@jonnyro.com>
2011-04-05 02:04:14 -0400

    Add skeleton modules.
    Signed-off-by: Jonathan S. Romero <jonnyro@jonnyro.com>

---
+++ client/netmax2int.py
@@ -0,0 +1,9 @@
+def max2ase(target,source,env):
+	tgt = str(target[0])
+	src = str(source[0])
+	
+	print "Target: %s" % tgt
+	print "Source: %s" %src
+	
+if __name__ == "__main__":
+	max2ase(["output.ase"],["input.max"],None)
