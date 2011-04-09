5c462c85da644de74a43063a6c4e3f1d36d40cf2
Jonathan S. Romero <jonnyro@jonnyro.com>
2011-04-09 12:31:55 -0400

    png2dds.py - fixup unix path to windows path when looking for nvdxt.
    unit_tests/png2dds_converter_test.py - create unit test for png2dds conversion module.
    
    Signed-off-by: Jonathan S. Romero <jonnyro@jonnyro.com>

--- common/png2dds.py
+++ common/png2dds.py
@@ -1,2 +1,13 @@
+import os
+
 def png2dds(input_file,output_file):
-	pass
+	path_to_nvdxt = '../third_party/nvdxt.exe'
+	
+	#Convert path into windows form
+	path_to_nvdxt = os.path.normpath(path_to_nvdxt)
+	
+	cmd = "%s -file %s -output %s" % (path_to_nvdxt,input_file,output_file)
+	print cmd
+	ret = os.system(cmd)
+	
+	return ret
